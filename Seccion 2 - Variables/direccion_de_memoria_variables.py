f633a8425fcd00ad36f4d0aa60e22befc002d1db
LaureanoVera <laureanoigvera@gmail.com>
2021-06-15 20:59:32 -0300

    seccion 2

---
+++ Seccion 2 - Variables/direccion_de_memoria_variables.py
@@ -0,0 +1,7 @@
+x = 10
+y = 5
+z = x + y
+
+print('x', id(x))
+print('y', id(y))
+print('z', id(z))
---
+++ Seccion 2 - Variables/variables_python.py
@@ -0,0 +1,6 @@
+my_name = 'Laureano Vera'
+my_age = 18
+my_boolean = True
+print(my_name)
+print(my_age)
+print(my_boolean)
