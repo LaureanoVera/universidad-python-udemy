5942fdc11a48144db0ea2ed563559984e133bc74
LaureanoVera <laureanoigvera@gmail.com>
2021-06-15 21:18:07 -0300

    manejo de cadenas 2

---
+++ Seccion 3 - Tipos de Datos/manejo_cadenas_dos.py
@@ -0,0 +1,13 @@
+#Concatenar 2
+string = 'random text'
+comment = 'the best comment'
+print(string, comment)
+
+#Sumar string
+number_one = '1'
+number_two = 2
+print(int(number_one) + number_two)
+
+# number_two = 1
+# number_two = 2
+# print(number_one + number_two)
