f9424b6734edec50a30378e671f46132d6c49381
LaureanoVera <laureanoigvera@gmail.com>
2021-06-15 21:11:43 -0300

    manejo de cadenas

---
+++ Seccion 3 - Tipos de Datos/manejo_cadenas.py
@@ -0,0 +1,7 @@
+#Cadenas (String)
+my_favorite_artist = 'Lil Mosey'
+my_favorite_artist_arg = 'Duki'
+
+#Concatenar
+print('Mis artistas favoritos: ' + my_favorite_artist_arg + ' y ' + my_favorite_artist)
+
