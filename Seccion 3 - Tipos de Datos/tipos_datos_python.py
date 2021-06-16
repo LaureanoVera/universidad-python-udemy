bd6c9de4cb968b5c02ed5579e5f9e00b79d23408
LaureanoVera <laureanoigvera@gmail.com>
2021-06-15 21:05:59 -0300

    tipos de datos

---
+++ Seccion 3 - Tipos de Datos/tipos_datos_python.py
@@ -0,0 +1,15 @@
+var_int = 10
+print(var_int)
+print('TYPE: ', type(var_int))
+
+var_float = 10.4
+print(var_float)
+print('TYPE: ', type(var_float))
+
+var_str = 'string'
+print(var_str)
+print('TYPE: ', type(var_str))
+
+var_bool = True #False
+print(var_bool)
+print('TYPE: ', type(var_bool))
