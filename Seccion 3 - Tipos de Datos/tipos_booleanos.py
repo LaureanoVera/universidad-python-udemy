375fe313d3d8398bab3a4313108bac95aeb478b7
LaureanoVera <laureanoigvera@gmail.com>
2021-06-17 20:56:37 -0300

    tipos booleanos

---
+++ Seccion 3 - Tipos de Datos/tipos_booleanos.py
@@ -0,0 +1,12 @@
+# Tipos bool (boolean)
+print('=== === Bool === ===')
+my_true = True
+my_false = False
+my_test = 3 > 2
+
+if my_test:
+  print('my_test is true')
+else:
+  print('my_test is false')
+
+print(my_true)
