808e4e46c0035cefe36c89ebf1aae22d1de0b7c3
LaureanoVera <laureanoigvera@gmail.com>
2021-06-18 10:04:16 -0300

    ejercicio operador not

---
+++ Seccion 4 - Operadores en Python/ejercicio_operador_not.py
@@ -0,0 +1,10 @@
+print('### Ejercicio not ###')
+vacaciones = False
+dia_franco = True
+
+if not(vacaciones or dia_franco):
+  print('Tiene deberes que hacer')
+else:
+  print('Puede asistir al juego')
+
+print('### #####  ##### ###')
