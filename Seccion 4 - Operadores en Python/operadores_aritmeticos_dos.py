dd3a04f6e906cbab374bbfb677652e1c848112b6
LaureanoVera <laureanoigvera@gmail.com>
2021-06-17 21:25:52 -0300

    operadores aritmeticos dos

---
+++ Seccion 4 - Operadores en Python/operadores_aritmeticos_dos.py
@@ -0,0 +1,14 @@
+print('### Operadores ###')
+a = 2
+b = 5
+multiplicacion = a * b
+division = b / a
+division_int = b // a
+modulo = b % a
+exponente = a ** b
+print(f'result: {multiplicacion}')
+print(f'result: {division}')
+print(f'result: {division_int}')
+print(f'result: {modulo}')
+print(f'result: {exponente}')
+print('### ####  #### ###')
