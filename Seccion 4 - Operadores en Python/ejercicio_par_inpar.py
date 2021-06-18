f723e885fd0d2f5e6fe3d9904ce08616a4d507ab
LaureanoVera <laureanoigvera@gmail.com>
2021-06-18 08:13:45 -0300

    ejercicio par e impar

---
+++ Seccion 4 - Operadores en Python/ejercicio_par_inpar.py
@@ -0,0 +1,10 @@
+print('### Ejercicio par e impar ###')
+
+a = int(input('Writte a number: '))
+
+if a % 2 == 0:
+  print('Pair number')
+else:
+  print('Odd number')
+
+print('### ##### ##### ##### ###')
