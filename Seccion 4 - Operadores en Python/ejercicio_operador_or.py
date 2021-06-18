5bfe0aa25c66eeef8aeb95b8a5044f9e8609c40f
LaureanoVera <laureanoigvera@gmail.com>
2021-06-18 10:02:37 -0300

    ejercicio operador or

---
+++ Seccion 4 - Operadores en Python/ejercicio_operador_or.py
@@ -0,0 +1,9 @@
+print('### Ejercicio or ###')
+vacaciones = False
+dia_franco = True
+
+if vacaciones or dia_franco:
+  print('Puede asistir al juego')
+else:
+  print('Tiene deberes que hacer')
+print('### #####  ##### ###')
