cc4a4ee3989af4966d1ae384514b41594473411e
LaureanoVera <laureanoigvera@gmail.com>
2021-06-18 08:17:24 -0300

    ejercicio mayor de edad

---
+++ Seccion 4 - Operadores en Python/ejercicio_es_mayor.py
@@ -0,0 +1,7 @@
+print('### Ejercicio par e impar ###')
+user_age = int(input('Your age: '))
+if user_age >= 18:
+  print('You are of age')
+else:
+  print('You are not of legal age')
+print('### ##### ##### ##### ###')
