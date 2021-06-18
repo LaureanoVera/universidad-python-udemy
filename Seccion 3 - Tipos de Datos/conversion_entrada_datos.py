a9e9eecc927121c543e5476ae5bdb385f50c69d6
LaureanoVera <laureanoigvera@gmail.com>
2021-06-17 21:07:13 -0300

    conversion de entrada de datos

---
+++ Seccion 3 - Tipos de Datos/conversion_entrada_datos.py
@@ -0,0 +1,7 @@
+print('=== === input2 === ===')
+num1 = int(input('number one: '))
+num2 = int(input('number two: '))
+result = num1 + num2
+print('result:', result)
+
+print('=== === ====== === ===')
