0e4b5d93d105bad5fce76e0701de8c6f6053487b
LaureanoVera <laureanoigvera@gmail.com>
2021-06-23 17:28:07 -0300

    clase Persona parte 2

--- Seccion 9 - Clases y objetos/Persona.py
+++ Seccion 9 - Clases y objetos/Persona.py
@@ -1,4 +1,12 @@
 class Persona:
-  pass
+  mi_atributo = ''
+  def __init__(self):
+    self.nombre = 'Laureano'
+    self.apellido = 'Vera'
+    self.edad = 18
 
+persona1 = Persona()
 print(type(Persona))
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
