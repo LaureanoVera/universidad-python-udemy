c0b9e4de43f8bee6dcdbefe68ae4f27916a1e4bf
LaureanoVera <laureanoigvera@gmail.com>
2021-06-23 11:33:33 -0300

    tuplas en python

---
+++ Seccion 7 - Colleciones en python/tuplas_python.py
@@ -0,0 +1,12 @@
+# Definir una tupla
+# si solo tiene un elemento debe tener coma = ('apple',)
+fruits = ('apple', 'orange', 'pear')
+# saber el largo
+print(len(fruits))
+# acceder a un numero
+print(fruits[0])
+# navegacion inversa
+print(fruits[-1])
+# acceder a un rango
+print(fruits[0:])
+
