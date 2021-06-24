e6176bf9f36ce1d39f6c101479039c5ff3eff0bf
LaureanoVera <laureanoigvera@gmail.com>
2021-06-24 10:49:27 -0300

    metodos get set

---
+++ Seccion 10 - Encapsulamiento en python/metodos_get_set.py
@@ -0,0 +1,20 @@
+class Persona:
+  def __init__(self, name, last, age):
+    self._nombre = name
+    self.apellido = last
+    self.edad = age
+
+  @property
+  def nombre(self):
+    print('Llamando metodo get nombre')
+    return self._nombre
+
+  @nombre.setter
+  def nombre(self, nombre):
+    print('Llamando metodo set nombre')
+    self._nombre = nombre
+
+laureano = Persona('Laureano', 'Vera', 18)
+print(laureano.nombre)
+laureano.nombre = 'Lauri'
+print(laureano.nombre)
