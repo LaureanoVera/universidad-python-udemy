74d8ae88ba06fb4f401e71d34f528caba8dc0a09
LaureanoVera <laureanoigvera@gmail.com>
2021-06-27 17:04:56 -0300

    ClientePersona

---
+++ Seccion 11 - Herencia en Python/ClientePersona.py
@@ -0,0 +1,7 @@
+from Persona import *
+
+persona_dos = Persona('Ivan', 19)
+print(persona_dos)
+
+empleado_dos = Empleado('Gerardo', 20, 64000)
+print(empleado_dos)
--- Seccion 11 - Herencia en Python/Persona.py
+++ Seccion 11 - Herencia en Python/Persona.py
@@ -3,10 +3,16 @@ class Persona:
     self.nombre = nombre
     self.edad = edad
 
+  def __str__(self):
+    return f'Persona: {self.nombre}, Edad: {self.edad}'
+
 class Empleado(Persona):
   def __init__(self, nombre, edad, sueldo):
     super().__init__(nombre, edad)
     self.sueldo = sueldo
+
+  def __str__(self):
+      return f'{super().__str__()}, Sueldo: {self.sueldo}'
 
 empleado_uno = Empleado('Laureano', 18, 45000)
 print(empleado_uno.nombre)
