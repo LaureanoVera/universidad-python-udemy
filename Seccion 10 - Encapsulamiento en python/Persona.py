2f49c6f2d6fa613ec1e35dc0faeebb6a640503bc
LaureanoVera <laureanoigvera@gmail.com>
2021-06-27 10:04:51 -0300

    uso de clases y modulos

---
+++ Seccion 10 - Encapsulamiento en python/Persona.py
@@ -0,0 +1,32 @@
+class Persona:
+  def __init__(self, name, last, age):
+    self._nombre = name
+    self._apellido = last
+    self._edad = age
+
+  @property
+  def nombre(self):
+    print('Llamando metodo get nombre')
+    return self._nombre
+
+  @nombre.setter
+  def nombre(self, nombre):
+    print('Llamando metodo set nombre')
+    self._nombre = nombre
+
+  @property
+  def apellido(self):
+    return self._apellido
+    
+  @apellido.setter
+  def apellido(self, apellido):
+    self._apellido = apellido
+
+  @property
+  def edad(self):
+    return self._edad
+
+  @edad.setter
+  def edad(self, edad):
+    self._edad = edad
+
---
+++ Seccion 10 - Encapsulamiento en python/uso_clases_modulos.py
@@ -0,0 +1,6 @@
+from Persona import Persona
+
+persona_uno = Persona('Laureano', 'Vera', 18)
+
+print(persona_uno.nombre)
+
