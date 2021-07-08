e47cbb8d5269b86219b5ca70b71a95bc2542a1ae
LaureanoVera <laureanoigvera@gmail.com>
2021-07-08 08:37:43 -0300

    test figura geometrica

--- test_figura_geometrica.py
+++ test_figura_geometrica.py
@@ -1,7 +1,8 @@
 from FiguraGeometrica2 import Cuadrado
 
 cuadrado_uno = Cuadrado(5, 'rojo')
-print(cuadrado_uno.calcular_area())
+# print(cuadrado_uno.calcular_area())
+print(cuadrado_uno)
 
 # MRO - Method Resolution Order
-print(Cuadrado.mro())
+# print(Cuadrado.mro())
