99343a8635a791314b97f9ea21706c0b341786ef
LaureanoVera <laureanoigvera@gmail.com>
2021-07-08 08:19:22 -0300

    herencia multiple en python 3

---
+++ test_figura_geometrica.py
@@ -0,0 +1,7 @@
+from FiguraGeometrica2 import Cuadrado
+
+cuadrado_uno = Cuadrado(5, 'rojo')
+print(cuadrado_uno.alto)
+print(cuadrado_uno.ancho)
+print(cuadrado_uno.color)
+print(cuadrado_uno.calcular_area())
