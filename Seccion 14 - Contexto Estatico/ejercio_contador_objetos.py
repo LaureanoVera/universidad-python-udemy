e6733fdb40df57c671fd4c8349e836a0f4e83570
LaureanoVera <laureanoigvera@gmail.com>
2021-07-23 19:41:39 -0300

    mejora ejercicio contador objetos

--- Seccion 14 - Contexto Estatico/ejercio_contador_objetos.py
+++ Seccion 14 - Contexto Estatico/ejercio_contador_objetos.py
@@ -1,9 +1,13 @@
 class Persona:
   contador_personas = 0
 
+  @classmethod
+  def generar_valor(cls):
+    cls.contador_personas += 1
+    return cls.contador_personas
+
   def __init__(self,nombre,edad):
-    Persona.contador_personas += 1
-    self.id = Persona.contador_personas
+    self.id = Persona.generar_valor()
     self.name = nombre
     self.age = edad
 
@@ -13,3 +17,6 @@ class Persona:
 
 persona1 = Persona('Laureano', 18)
 print(persona1)
+persona2 = Persona('Lauri', 19)
+print(persona2)
+print(Persona.contador_personas)
