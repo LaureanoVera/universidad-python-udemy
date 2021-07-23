e444a56957859935d2a0f646c216bbd4ba20d0dd
LaureanoVera <laureanoigvera@gmail.com>
2021-07-23 19:01:05 -0300

    variables de clase

---
+++ Seccion 14 - Contexto Estatico/variables_de_clase.py
@@ -0,0 +1,14 @@
+class MiClase:
+    variable_clase = "valor variable clase"
+
+    def __init__(self, variable_instancia):
+        self.variable_instancia = variable_instancia
+
+
+print(MiClase.variable_clase)
+miClase = MiClase('Valor variable instancia')
+print(miClase.variable_instancia)
+print(miClase.variable_clase)
+
+miClase2 = MiClase('Otro valor de variable instancia')
+print(miClase2.variable_instancia)
