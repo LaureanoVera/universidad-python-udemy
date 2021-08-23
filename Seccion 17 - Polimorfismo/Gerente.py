6c76f06006f20831945df6ba54d841a855b5fd71
LaureanoVera <laureanoigvera@gmail.com>
2021-08-23 19:49:28 -0300

    polimorfismo parte 1

---
+++ Seccion 17 - Polimorfismo/Gerente.py
@@ -0,0 +1,9 @@
+from Empleado import Employee
+
+class Manager(Employee):
+  def __init__(self, name, salary, department):
+      super().__init__(name, salary)
+      self.department = department
+
+      def __str__(self):
+        return f'Manager: [Department: {self.department}] {super().__str__()}'
