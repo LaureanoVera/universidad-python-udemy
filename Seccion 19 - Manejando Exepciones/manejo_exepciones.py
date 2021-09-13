5c21f635d0489b7146f64994f5ae4ba8d5682fd8
LaureanoVera <laureanoigvera@gmail.com>
2021-09-13 17:54:22 -0300

    procesar clases de exepcion mas especificas

--- Seccion 19 - Manejando Exepciones/manejo_exepciones.py
+++ Seccion 19 - Manejando Exepciones/manejo_exepciones.py
@@ -1,13 +1,18 @@
 result = None
 
-x = 10
+x = '10'
 y = 0
 
 try:
     result = x / y
+
+except ZeroDivisionError as zde:
+    print(f'An error occurred: {zde}')
+except TypeError as te:
+    print(f'An error occurred: {te}')
 except Exception as e:
     print(f'An error occurred: {e}')
-# except ZeroDivisionError as e:
-#     print(f'An error occurred: {e}')
+
+
 print(f'Result: {result}')
 print('Countinue...')
