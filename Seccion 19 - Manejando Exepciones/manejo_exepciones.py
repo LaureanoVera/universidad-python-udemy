c46371fe1dc410441038121f526cbd733ff859bb
LaureanoVera <laureanoigvera@gmail.com>
2021-09-17 21:36:27 -0300

    python university: else, finally blocks in exceptions

--- Seccion 19 - Manejando Exepciones/manejo_exepciones.py
+++ Seccion 19 - Manejando Exepciones/manejo_exepciones.py
@@ -4,14 +4,16 @@ try:
     x = int(input('First Number: '))
     y = int(input('Second Number: '))
     result = x / y
-
 except ZeroDivisionError as zde:
     print(f'An error occurred: {zde}')
 except TypeError as te:
     print(f'An error occurred: {te}')
 except Exception as e:
     print(f'An error occurred: {e}')
-
+else:
+    print('No error ocurred')
+finally:
+    print('Ejecution finally block')
 
 print(f'Result: {result}')
 print('Countinue...')
