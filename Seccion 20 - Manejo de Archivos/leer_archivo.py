a47d350e9616708dfb6636203101e6949cb859c0
LaureanoVera <laureanoigvera@gmail.com>
2021-09-18 18:34:10 -0300

    universidad python: mas formas de trabajar con archivos en python

--- Seccion 20 - Manejo de Archivos/leer_archivo.py
+++ Seccion 20 - Manejo de Archivos/leer_archivo.py
@@ -1,12 +1,23 @@
-try:
-  file = open('./Seccion 20 - Manejo de Archivos/test.txt', 'r')
-  # print(file.read())
-  # print(file.read(4))
-  print(file.readline())
-  print(file.readline())
-except Exception as e:
-  print(e)
-finally:
-  file.close()
-  print('\n')
-  print('Close file')
+# try:
+#   file = open('./Seccion 20 - Manejo de Archivos/test.txt', 'r')
+#   # print(file.read())
+#   # print(file.read(4))
+#   # print(file.readline())
+# except Exception as e:
+#   print(e)
+# finally:
+#   file.close()
+#   print('\n')
+#   print('Close file')
+
+file = open('./Seccion 20 - Manejo de Archivos/test.txt', 'r')
+file_two = open('./Seccion 20 - Manejo de Archivos/copy.txt', 'a')
+
+# # iterar el archivo
+# for line in file:
+#   print(line)
+
+# a - anexar informacion
+file_two.write(file.read())
+file_two.close()
+print('End Proccess')
